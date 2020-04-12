8674b071a488cc9faedf55a322f94f4e23f0cb4a
Toby Boyne <tobyboyne@gmail.com>
2020-04-12 15:01:32 +0100

    Scrabble combinations made more efficient

--- scrabble.py
+++ scrabble.py
@@ -1,39 +1,52 @@
 """For the tile frequency and values given in scrabble-values.txt, how many unique ways can you chose a
-7-letter hand worth exactly 46 points?"""
+7-letter hand worth exactly 46 points?
+
+Key insights:
+ - Must begin with 10, 10, 8, 8 to reach 46 points
+ - Remaining combinations to get to exactly 46 are shown in COMBS
+ - There are enough copies of each letter below 8-points to not worry about the count of each tile"""
 
 import numpy as np
 from parse import parse
+from itertools import combinations_with_replacement
 
 def get_values():
 	with open('txt-files/scrabble-values.txt') as f:
-		tile_values = np.zeros((27, 2))
+		tile_values = np.zeros((27, 2), dtype=int)
+		grouped_letters = {}
 		for i, line in enumerate(f):
-			_, *count_value = parse('{},{:d},{:d}', line.strip('\n'))
-			tile_values[i, :] = count_value
-	return tile_values
+			letter, count, value = parse('{},{:d},{:d}', line.strip('\n'))
+			tile_values[i, :] = [count, value]
+			grouped_letters[value] = grouped_letters.get(value, []) + [letter]
+	print(grouped_letters)
+	return tile_values, grouped_letters
 
 
-def find_all_combinations(points, used, target):
-	# points must be sorted
-	if len(used) == 7:
-		return []
+def combs_to_target(points, r, target):
+	return (comb for comb in combinations_with_replacement(points, r) if sum(comb) == target)
+
+def find_all_combinations(points):
+	COMBS = combs_to_target((1, 2, 3, 4, 5), 3, 10)
 
 	combinations = []
-	for i, p in enumerate(points):
-		if p == target and len(used) == 6:
-			combinations.append(used + [p])
-		if p <= target:
-			remaining = points[:i]
-			combinations += find_all_combinations(remaining, used + [p], target - p)
+	for comb in COMBS:
+		sub_combinations = ['']
+		for p in set(comb):
+			new_subs = []
+			for i, sub in enumerate(sub_combinations):
+				for new_letters in combinations_with_replacement(points[p], comb.count(p)):
+					new_subs.append(sub + ''.join(new_letters))
+			sub_combinations = new_subs
+		combinations += sub_combinations
 
 	return combinations
 
 
 
-def num_combinations(tile_values):
-	points = list(sorted(tile_values[:, 1]))
-	print(points)
-	print(find_all_combinations(points, [], 46))
+def brute_force(tile_values, grouped_letters):
+	all_combs = find_all_combinations(grouped_letters)
+	print(all_combs)
+	return len(all_combs)
 
-tile_values = get_values()
-num_combinations(tile_values)
+tile_values, grouped_letters = get_values()
+print(brute_force(tile_values, grouped_letters))

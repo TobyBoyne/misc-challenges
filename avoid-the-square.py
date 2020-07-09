5eba438d9385a53db7694d5bbb4d1357bae1f4fe
Toby Boyne <tobyboyne@gmail.com>
2020-07-09 18:08:08 +0100

    Found all solutions to puzzle

--- avoid-the-square.py
+++ avoid-the-square.py
@@ -1,31 +1,33 @@
 from tqdm import tqdm
 import numpy as np
-from itertools import product
+from itertools import product, combinations
+from math import comb
 
 """
-0 -> not assigned
-1 -> player 1
-2 -> player 2
+https://www.think-maths.co.uk/avoidthesquare
 """
 
 W = 5
-# N is the number of tiles to be placed by player 1
-N = np.ceil(W ** 2 / 2)
-print(N)
+# N is the number of pieces placed by player 2
+N = W ** 2 // 2
 
-def find_blocked_tiles(b):
-	"""Finds all tiles that are not valid as would form a square"""
-	return b
+class Combiner:
+	def __init__(self, *args):
+		self._it = combinations(*args)
+		self._len = comb(W**2, N)
+
+	def __iter__(self):
+		return self._it
+
+	def __len__(self):
+		return self._len
 
 def get_all_squares():
-	"""Returns a dictionary of form
-	(x, y): list of square masks that include tile at (x, y)"""
-	mask_dict = {(x, y): [] for (x, y) in product(range(W), repeat=2)}
+	"""Returns a list of all_masks"""
+	all_masks = []
 	ROT = np.array([[0, 1], [-1, 0]])
 	square_vectors = [np.array([y, 1]) for y in range(0, W)]
 	square_vectors += [np.array([1, x]) for x in range(2, W)]
-
-	print(np.dot(ROT, np.array([1, 2])))
 
 	for vector in square_vectors:
 		for r in range(1, W):
@@ -34,49 +36,65 @@ def get_all_squares():
 			# only go through (x, y) where the square's bounding box is within the board
 			for y, x in product(range(0, W-dy-dx), range(dy, W-dx)):
 				cur_pos = np.array([y, x], dtype=np.int)
-				mask = np.zeros((W, W))
+				mask = np.zeros((W, W), dtype=np.bool)
 				for t in range(4):
 					# try:
 					# 	mask[tuple(cur_pos)] = 1
 					# except IndexError:
 					# 	print(f"{mask}\n{x=},{y=}\n{move_vec=}\n{cur_pos=}")
 					# 	raise ValueError
-					mask[tuple(cur_pos)] = 1
+					pos_tuple = tuple(cur_pos)
+					mask[pos_tuple] = True
 
 					cur_pos += move_vec
 					move_vec = np.dot(ROT, move_vec)
+				all_masks.append(mask)
 
-				print(mask)
-
-
-
-	# squares = [unit_square * r for r in range(1, W+1)]
-	# print(squares)
-	return mask_dict
+	return all_masks
 
 
-get_all_squares()
+def gen_all_boards():
+	tqdm_iter = tqdm(Combiner(range(W**2), N))
+	boards = []
+	for pieces in tqdm_iter:
+		board = np.zeros(25, dtype=np.bool)
+		for i in pieces:
+			board[i] = True
+
+		boards.append(board.reshape((W, W)))
+	print(boards[0])
+	return boards
+
+
+def no_squares(board):
+	for mask in all_masks:
+		prod = np.logical_and(board, mask)
+		if np.count_nonzero(prod) == 4:
+			return False
+	return True
 
 
 
-empty_board = np.zeros((W, W), dtype=np.int)
+all_masks = get_all_squares()
+boards = gen_all_boards()
 
-prev_boards = [empty_board]
-# filled_boards contained all boards where player 1 has not made a square
-filled_boards = []
+valid_boards = []
+all_boards_iter = tqdm(boards)
+valid_count = 0
+for i, board in enumerate(all_boards_iter):
+	if no_squares(board):
+		valid_boards.append(board)
+		valid_count += 1
 
-# while prev_boards:
-# 	cur_boards = prev_boards
-# 	prev_boards = []
-# 	for board in cur_boards:
-# 		if board.sum() == N:
-# 			filled_boards.append(board)
-# 		else:
-# 			for (x, y) in product(range(3), 2):
-# 				if board[y, x] == 0:
-# 					board[y, x] = 1
-# 					new_board = find_blocked_tiles(board)
-# 					prev_boards.append(new_board)
-#
-#
+	if i % 10000 == 0:
+		all_boards_iter.set_description(f"{valid_count / (i+1)}")
 
+
+valid_boards_iter = tqdm(valid_boards)
+solutions = []
+for board in valid_boards_iter:
+	inverse_board = np.logical_not(board)
+	if no_squares(inverse_board):
+		solutions.append(inverse_board)
+
+print(solutions)
